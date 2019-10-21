293464200d9ce7217c50aea2d6e8f644f099b492
NefiteTifall <nefitetifall@gmail.com>
2019-10-21 17:34:48 +0200

    Finish work 21/10/2019

--- JS/alertbox.js
+++ JS/alertbox.js
@@ -7,6 +7,7 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
     modal.style.display = "block";
     modal.style.animationName = "start";
+    document.getElementById('imput').value = ''
 }
 span.onclick = function() {
     modal.style.display = "none";
